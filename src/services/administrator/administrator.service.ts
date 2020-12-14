4e32d9a8a1fce9569ddb9e591ace20fa259f348b
aleksandar83p <alex5ko83@gmail.com>
2020-12-14 13:36:37 +0100

    Implement an global API response mechanism

--- src/services/administrator/administrator.service.ts
+++ src/services/administrator/administrator.service.ts
@@ -5,6 +5,8 @@ import { AddAdministratorDto } from 'src/dtos/administrator/add.administrator.dt
 import { Repository } from 'typeorm';
 import * as crypto from 'crypto';
 import { EditAdministratorDto } from 'src/dtos/administrator/edit.administrator.dto';
+import { ApiResponse } from 'src/misc/api.response.class';
+import { resolve } from 'path';
 
 
 @Injectable()
@@ -22,7 +24,7 @@ export class AdministratorService {
         return this.administrator.findOne(id);
     }
 
-    add(data: AddAdministratorDto): Promise<Administrator>{
+    add(data: AddAdministratorDto): Promise<Administrator | ApiResponse>{
         const passwordHash = crypto.createHash('sha512');
         passwordHash.update(data.password);
 
@@ -32,11 +34,24 @@ export class AdministratorService {
         newAdmin.username = data.username;
         newAdmin.passwordHash = passwordHashString;
 
-        return this.administrator.save(newAdmin);
+        return new Promise((resolve) => {
+            this.administrator.save(newAdmin)
+            .then(data => resolve(data))
+            .catch(error => {
+                const response: ApiResponse = new ApiResponse("error", -1001);
+                resolve(response);
+            });
+        });
     }
 
-    async editById(id: number, data: EditAdministratorDto): Promise<Administrator>{
+    async editById(id: number, data: EditAdministratorDto): Promise<Administrator | ApiResponse>{
         let admin: Administrator = await this.administrator.findOne(id);
+
+        if(admin === undefined){
+            return new Promise((resolve) => {
+                resolve(new ApiResponse("error", -1002));
+            })
+        }
 
         const passwordHash = crypto.createHash('sha512');
         passwordHash.update(data.password);
